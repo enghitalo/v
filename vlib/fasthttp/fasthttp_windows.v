6812cd11a690452449a9d6743ff266a3ec69a400
Hitalo Souza <enghitalo@gmail.com>
2026-01-06 23:16:18 +0000

    fix: Windows compilation error

--- vlib/fasthttp/fasthttp_windows.v
+++ vlib/fasthttp/fasthttp_windows.v
@@ -1,15 +1,5 @@
 module fasthttp
 
-// new_server creates and initializes a new Server instance.
-pub fn new_server(config ServerConfig) !&Server {
-	mut server := &Server{
-		port:            config.port
-		request_handler: config.handler
-	}
-
-	return server
-}
-
 // run starts the server and begins listening for incoming connections.
 pub fn (mut server Server) run() ! {
 	println('TODO: implement fasthttp.Server.run on windows')
